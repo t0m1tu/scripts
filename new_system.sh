ea672eb21c4e5660db4d9f46160d7899038bbb67
MQ <33011060+t0m1tu@users.noreply.github.com>
2019-02-06 20:46:56 +0800

    Create new_system.sh

---
+++ new_system.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+OS=`uname -s`
+if [ ${OS} = "Darwin"  ];then
+    sudo brew install git vim zsh curl wget openssh
+elif [ ${OS} = "Linux"  ];then
+    if [ -f /etc/os-release ];then
+    # for the path if [ -x /etc/ ];then
+        source /etc/os-release
+        case $ID in
+            debian|ubuntu|devuan)
+                sudo apt-get install git vim zsh curl wget ssh unzip unrar
+                sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+                git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+                git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+                #sed -i '/git/a\\zsh-autosuggestions' ~/.zshrc
+                #sed -i '/git/a\\zsh-syntax-highlighting' ~/.zshrc
+                #sed -i '/git/a\\x' ~/.zshrc
+                #sed -i '/git/a\\sudo' ~/.zshrc
+                #sed -i '/git/a\\extract' ~/.zshrc
+                source ~/.zshrc
+                ssh-keygen
+                wget https://github.com/t0m1tu/ssr-backup/raw/master/ShadowsocksR-Python.zip
+
+                ;;
+            centos|fedora|rhel)
+                yumdnf="yum"
+                if test "$(echo "$VERSION_ID >= 22" | bc)" -ne 0;
+                then
+                    yumdnf="dnf"
+                fi
+                sudo $yumdnf install -y git vim zsh curl wget openssh
+                ;;
+            openwrt)
+                opkg update
+                ;;
+            *)
+                exit 1
+                ;;
+        esac
+    elif [ -x /data/data/com.termux/files/home ];then
+        pkg install git vim zsh curl wget openssh
+    fi
+else
+    echo "Other OS: ${OS}"
+fi
