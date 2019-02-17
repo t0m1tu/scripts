46740cc80b5da589a389e50d1d70afe3f030d198
t0m1tu <mzqgml@gmail.com>
2019-02-17 14:55:07 +0800

    ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python ~/ShadowsocksR/shadowsocks/local.py -c ~/ShadowsocksR/config.json
-echo "export http_proxy=http://127.0.0.1:8118" >> /etc/enviroment
-echo "export https_proxy=http://127.0.0.1:8118" >> /etc/enviroment
+export http_proxy=http://127.0.0.1:8118
+export https_proxy=http://127.0.0.1:8118
 sudo /etc/init.d/privoxy start
+python ~/ShadowsocksR/shadowsocks/local.py -c ~/ShadowsocksR/config.json
